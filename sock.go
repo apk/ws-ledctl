694bfe0d7c6a1515d893850e6df30d19827184bb
Andreas Krey <a.krey@gmx.de>
2015-09-07 20:18:19 +0200

    sock: complete relay/led ctl via websocket

--- sock.go
+++ sock.go
@@ -4,12 +4,17 @@ package main
 
 import (
 	"flag"
+	"os/exec"
+	"io/ioutil"
+	"fmt"
 	"github.com/gorilla/websocket"
 	"go/build"
 	"log"
 	"net/http"
 	"path/filepath"
 	"text/template"
+
+	"iow"
 )
 
 type hub struct {
@@ -35,7 +40,18 @@ func newHub() *hub {
 	}
 }
 
+func onezero(x int) int {
+	if (x == 0) {
+		return 0;
+	}
+	return 1;
+}
+
 func (h *hub) run() {
+	var r, g int;
+	d := 0xf000
+	od := 0
+	w := iow.Open()
 	for {
 		select {
 		case c := <-h.register:
@@ -46,6 +62,28 @@ func (h *hub) run() {
 				close(c.send)
 			}
 		case m := <-h.broadcast:
+			fmt.Printf("M: %s\n", m)
+			if len(m) > 0 {
+				switch m[0] {
+				case 'r':
+					r ^= 1
+					ioutil.WriteFile("/sys/class/gpio/gpio22/value",[]byte(fmt.Sprintf("%d", r)), 0644)
+				case 'g':
+					g ^= 1
+					ioutil.WriteFile("/sys/class/gpio/gpio17/value",[]byte(fmt.Sprintf("%d", g)), 0644)
+				case 'd':
+					d ^= 0x4000
+				case 't':
+					d ^= 0x1000
+				case 'l':
+					d ^= 0x2000
+				}
+			}
+			m = []byte(fmt.Sprintf("%dr%dg%dd%dt%dl", r, g,
+				onezero(d & 0x4000),
+				onezero(d & 0x1000),
+				onezero(d & 0x2000)))
+			fmt.Printf("M: %s\n", m)
 			for c := range h.connections {
 				select {
 				case c.send <- m:
@@ -54,6 +92,10 @@ func (h *hub) run() {
 					close(c.send)
 				}
 			}
+		}
+		if (d != od) {
+			fmt.Printf("%d %x\n", w, d);
+			iow.Set(w, d)
 		}
 	}
 }
@@ -129,13 +171,47 @@ func homeHandler(c http.ResponseWriter, req *http.Request) {
 	homeTempl.Execute(c, req.Host)
 }
 
+func picserve(ch chan chan []byte) {
+	for rq := range ch {
+		cmd := exec.Command("raspistill", "-t", "5", "-mm", "matrix", "-o", "-")
+		out, err := cmd.Output()
+		if err != nil {
+			log.Print("Exec:", err)
+		}
+		rq <- out
+	}
+}
+
 func main() {
 	flag.Parse()
 	homeTempl = template.Must(template.ParseFiles(filepath.Join(*assets, "home.html")))
 	h := newHub()
 	go h.run()
+
 	http.HandleFunc("/go/", homeHandler)
 	http.Handle("/go/ws", wsHandler{h: h})
+
+	http.HandleFunc("/go/set", func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err == nil {
+			h.broadcast <- body
+		}
+	})
+
+	ch := make(chan chan []byte)
+
+	go picserve(ch)
+
+	http.HandleFunc("/go/pic", func(w http.ResponseWriter, r *http.Request) {
+		_, err := ioutil.ReadAll(r.Body)
+		if err == nil {
+			rc := make(chan []byte)
+			ch <- rc
+			s := <-rc
+			w.Write([]byte(s))
+		}
+	})
+
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
