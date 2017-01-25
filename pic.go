80bc16ff993138ab7a8898e8f2896f7210314e9b
Andreas Krey <a.krey@aquilius.com>
2017-01-25 20:22:00 +0100

    pic: add ssl listener

--- pic.go
+++ pic.go
@@ -10,6 +10,8 @@ import (
 )
 
 var addr = flag.String("addr", "127.0.0.1:4040", "http service address")
+var ssladdr = flag.String("tls", "", "http service address")
+var certpref = flag.String("cert-prefix", "", "prefix for cert files")
 var docroot = flag.String("path", ".", "http root directory")
 var iphead = flag.String("ip", "", "header for remote IP")
 var wellknown = flag.String("well-known", "banana.h.apk.li", "host for .well-known")
@@ -29,5 +31,10 @@ func main() {
 
 	gnord.PiCam(mux,"/pic")
 
+	if *ssladdr != "" {
+		go log.Fatal(http.ListenAndServeTLS(*ssladdr,
+			*certpref + "fullchain.pem", *certpref + "key.pem",
+			mux))
+	}
 	log.Fatal(http.ListenAndServe(*addr, mux))
 }
